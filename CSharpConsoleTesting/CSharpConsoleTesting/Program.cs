a4e939f32cb33d55be22474f03fe4b0fd786ffc9
BrentBrouwer <brent.brouwer@valor.world>
2023-04-24 14:35:41 +0200

    Check if a substring is in a List<string>

--- CSharpConsoleTesting/CSharpConsoleTesting/Program.cs
+++ CSharpConsoleTesting/CSharpConsoleTesting/Program.cs
@@ -10,6 +10,25 @@ namespace CSharpConsoleTesting
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Application Started");
+
+            // Check if a substring is detected in a list of strings
+            List<string> StringList = new List<string>();
+
+            StringList.Add("B1art");
+            StringList.Add("P2eter");
+            StringList.Add("P3iet");
+
+            foreach (string str in StringList) 
+            {
+                if (str.Contains("2"))
+                {
+                    Console.WriteLine(str);
+                }
+            }
+
+            // Prevent the Application from stopping
+            Console.ReadLine();
         }
     }
 }
